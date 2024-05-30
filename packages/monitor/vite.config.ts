4e2d3105314e01cc3b983df0cc99e2b1042ca2e6
caoxianglin.13 <caoxianglin.13@bytedance.com>
2024-05-30 16:56:23 +0800

    feat(monitor): 添加式例

--- packages/monitor/vite.config.ts
+++ packages/monitor/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
       entry: './src/index.ts',
-      formats: ['es', 'cjs', 'iife', 'umd'],
-      name: 'Monitor',
+      formats: ['es', 'cjs'],
       fileName: 'index',
     },
   },
+  plugins: [dts()],
 });
